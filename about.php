8000077d1d1685ef21670862814229e1e84578f4
{LeeDongHui} <{chleedonghui@gmail.com}>
2023-11-02 02:10:33 +0900

    my first commit

---
+++ about.php
@@ -0,0 +1,79 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>웹 해킹 실습</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="./css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="./css/pricing.css" rel="stylesheet">
+  </head>
+
+  <body>
+     <header class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom box-shadow">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-shield-fill-check" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M8 0c-.69 0-1.843.265-2.928.56-1.11.3-2.229.655-2.887.87a1.54 1.54 0 0 0-1.044 1.262c-.596 4.477.787 7.795 2.465 9.99a11.777 11.777 0 0 0 2.517 2.453c.386.273.744.482 1.048.625.28.132.581.24.829.24s.548-.108.829-.24a7.159 7.159 0 0 0 1.048-.625 11.775 11.775 0 0 0 2.517-2.453c1.678-2.195 3.061-5.513 2.465-9.99a1.541 1.541 0 0 0-1.044-1.263 62.467 62.467 0 0 0-2.887-.87C9.843.266 8.69 0 8 0zm2.146 5.146a.5.5 0 0 1 .708.708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 7.793l2.646-2.647z"/>
+            </svg>&nbsp;&nbsp;
+            <h5 class="my-0 mr-md-auto font-weight-normal">seKUrity</h5>
+            <nav class="my-2 my-md-0 mr-md-3">
+              <a class="p-2 text-dark" href="index.php">Home</a>
+              <a class="p-2 text-dark" href="about.php">About</a>
+              <a class="p-2 text-dark" href="board.php">Board</a>
+              <a class="p-2 text-dark" href="members.php">Members</a>
+            </nav>
+            <a class="btn <?php if(isset($_COOKIE["user_id"])) { echo "btn-outline-danger"; } else { echo "btn-outline-success"; } ?>" href="<?php if(isset($_COOKIE["user_id"])) { echo "cookie_logout.php"; } else { echo "login.php"; } ?>">
+			        <?php if(isset($_COOKIE["user_id"])) { echo "Logout"; } else { echo "Login"; } ?>
+		        </a>
+            <!--signup.php-->
+        </header>
+
+    <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
+      <h1 class="display-4">About</h1>
+      <hr>
+    </div>
+    
+    <div class="container">
+      <p class="text-dark">2023년 2학기 seKUrity 파일 업로드 & 쿠키 탈취 취약점 실습 페이지입니다.</p>
+      <p class="text-dark">게시판 글쓰기 기능, 파일 업로드 기능, 로그인 기능 등을 활용해 실습해보세요.</p>
+      <p class="text-dark">실습 페이지 만들기 정말 힘드네요... 수고하세요^^</p>
+      <br>
+      <div class="alert alert-dark" role="alert">
+          <strong>파일 업로드 취약점 공격 순서</strong><br>
+          <ol>
+              <li>서버 환경을 분석한다</li>
+              <li>허용된 파일을 업로드한다</li>
+              <li>Print 하는 간단한 스크립트 파일을 업로드 후 실행이 되는지 확인한다</li>
+              <li>웹 쉘 파일을 업로드한다</li>
+              <li>웹 쉘 파일을 찾아 운영체제 명령을 실행한다</li>
+          </ol>
+      </div>
+    </div>
+
+    <!-- Icons -->
+    <script src="https://unpkg.com/feather-icons/dist/feather.min.js"></script>
+    <script>
+      feather.replace()
+    </script>
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="./js/jquery-slim.min.js"><\/script>')</script>
+    <script src="./js/popper.min.js"></script>
+    <script src="./js/bootstrap.min.js"></script>
+    <script src="./js/holder.min.js"></script>
+    <script>
+      Holder.addTheme('thumb', {
+        bg: '#55595c',
+        fg: '#eceeef',
+        text: 'Thumbnail'
+      });
+    </script>
+  </body>
+</html>
---
+++ cookie_login.php
@@ -0,0 +1,56 @@
+<?php
+    $id = $_GET['id'];
+    $pw = $_GET['pw'];
+
+    $db_conn = mysqli_connect("127.0.0.1", "webhacking_db", "webhacking", "login");
+    if($db_conn == false) {
+        echo mysqli_connect_error();
+    }
+    $query = "select * from user where id='{$id}' and pw='{$pw}'";
+    $result = mysqli_query($db_conn, $query);
+    echo "query: {$query} <br>";
+
+    if($result == false) {
+        echo mysqli_error($db_conn);
+    } else {
+        $row = mysqli_fetch_array($result);
+        if($row) {
+            echo "Hello {$row['id']}, login success!";
+        } else {
+            echo "login failed";
+        }
+
+        mysqli_close($db_conn);
+    }
+	$id = $_GET['id'];
+	$pw = $_GET['pw'];
+
+	$db_conn = mysqli_connect("127.0.0.1", "webhacking_db", "webhacking", "login");
+	if ($db_conn == false) {
+		echo mysqli_connect_error();
+	}
+
+	$query = "select * from user where id='{$id}' and pw='{$pw}'";
+	$result = mysqli_query($db_conn, $query);
+	echo "query: {$query} <br>";
+
+	if ($result == false) {
+		echo mysqli_error($db_conn);
+	} else {
+		$row = mysqli_fetch_array($result);
+		if ($row) {
+			// 로그인 성공 시 쿠키 설정
+			setcookie("user_id", $row['id'], time() + 3600); // 쿠키 유효 시간: 1 시간
+
+			// 로그인 성공 시 index.php로 리디렉션
+			header("Location: index.php");
+			exit; // 리디렉션 후 코드 실행을 중지합니다.
+		} else {
+			// 로그인 실패 시 다시 로그인 페이지로 리디렉션
+			header("Location: login.php");
+			exit;
+		}
+
+		mysqli_close($db_conn);
+	}
+?>
